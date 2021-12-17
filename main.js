539350545b222bcb0da31a7dd88b84c1d89c09f3
Supercoder5157 <76742451+Supercoder5157@users.noreply.github.com>
2021-12-17 09:15:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startclassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/cWBRUMp7m/model.json",modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+     if (error) {
+         console.error(error);
+     }
+     else {
+        console.log(results);
+     }
+}
